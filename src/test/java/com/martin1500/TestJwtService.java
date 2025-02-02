d354095d6d253292343a0266f6103ce2146e4b84
Martinm1500 <mm389358@gmail.com>
2025-02-02 18:56:14 -0300

    The way to obtain the secretKey was modified.

--- src/test/java/com/martin1500/TestJwtService.java
+++ src/test/java/com/martin1500/TestJwtService.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.lang.reflect.Method;
@@ -19,8 +20,9 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @SpringBootTest(classes = TaskTrackerApplication.class)
 @ExtendWith(MockitoExtension.class)
 public class TestJwtService {
-    private final String secretKey= System.getenv("JWT_SECRET_KEY");
 
+    @Value("${jwt.secretKey:${JWT_SECRET_KEY}}")
+    private String secretKey;
     @Autowired
     private JwtService jwtService;
 
