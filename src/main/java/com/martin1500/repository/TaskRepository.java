a6ab924f88aa5a7b336e7cd31fd2e29ac7ededf1
Martinm1500 <mm389358@gmail.com>
2025-02-10 13:55:55 -0300

    @Repository annotations were added in UserRepository and TaskRepository

--- src/main/java/com/martin1500/repository/TaskRepository.java
+++ src/main/java/com/martin1500/repository/TaskRepository.java
@@ -2,8 +2,8 @@ package com.martin1500.repository;
 
 import com.martin1500.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
-@Service
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long > {
 }
