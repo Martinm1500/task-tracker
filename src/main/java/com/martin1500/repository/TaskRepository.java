395e89e31c90b6cfdfce0f1cbc8e3050b597100b
Martinm1500 <mm389358@gmail.com>
2025-02-10 16:19:46 -0300

    Query Method was added in TaskRepository

--- src/main/java/com/martin1500/repository/TaskRepository.java
+++ src/main/java/com/martin1500/repository/TaskRepository.java
@@ -1,9 +1,13 @@
 package com.martin1500.repository;
 
 import com.martin1500.model.Task;
+import com.martin1500.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long > {
+    List<Task> findByUserOrderByPriorityAscDueDateAsc(User user);
 }
