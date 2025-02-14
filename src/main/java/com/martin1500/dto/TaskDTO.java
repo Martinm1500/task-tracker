9ecdfea18858ac7611f16cd3ac05f1c0fb8ae018
Martinm1500 <mm389358@gmail.com>
2025-02-14 16:20:13 -0300

    Validations were added to TaskDTO

--- src/main/java/com/martin1500/dto/TaskDTO.java
+++ src/main/java/com/martin1500/dto/TaskDTO.java
@@ -3,6 +3,9 @@ package com.martin1500.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.martin1500.model.util.Priority;
 import com.martin1500.model.util.Status;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,12 +20,27 @@ import java.time.LocalDateTime;
 public class TaskDTO {
 
     private Long id;
+
+    @Size(max = 50, message = "Comments cannot exceed 50 characters")
     private String title;
+
+    @Size(max = 255, message = "Comments cannot exceed 255 characters")
     private String description;
+
+    @NotNull(message = "Status cannot be null")
     private Status status;
+
+    @NotNull(message = "Priority cannot be null")
     private Priority priority;
+
+    @Future(message = "Due date must be in the future")
+    @NotNull(message = "Due date cannot be null")
     private LocalDate dueDate;
+
     private LocalDateTime createdAt;
+
     private LocalDateTime updatedAt;
+
+    @Size(max = 255, message = "Comments cannot exceed 255 characters")
     private String comments;
 }
