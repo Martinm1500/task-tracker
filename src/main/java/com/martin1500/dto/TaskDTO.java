d21b4cf41df1a6274efa86e8ab9518c2af9222d9
Martinm1500 <mm389358@gmail.com>
2025-02-19 18:17:16 -0300

    Added NotArgsConstructor

--- src/main/java/com/martin1500/dto/TaskDTO.java
+++ src/main/java/com/martin1500/dto/TaskDTO.java
@@ -9,11 +9,13 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Data
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
